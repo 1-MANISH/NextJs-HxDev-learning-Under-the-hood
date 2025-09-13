f677c812d5720e1504e39149fcf73d0ae573bbf3
manish_patidar <shanimpatidar@gmail.com>
2025-09-13 15:52:09 +0530

    updated with modules css

--- 06-design/src/app/games/page.tsx
+++ 06-design/src/app/games/page.tsx
@@ -13,6 +13,19 @@ const Games = () => {
                         {
                                 Array.from({length:10}).map((_,index) => <p><Link key={index} href={`/games/${index}`}>Game {index}</Link></p>)
                         }
+
+                        
+                <div className="ai-matrix-loader">
+                        <div className="digit">0</div>
+                        <div className="digit">1</div>
+                        <div className="digit">0</div>
+                        <div className="digit">1</div>
+                        <div className="digit">1</div>
+                        <div className="digit">0</div>
+                        <div className="digit">0</div>
+                        <div className="digit">1</div>
+                        <div className="glow"></div>
+                </div>
                 </div>
         )
 }
