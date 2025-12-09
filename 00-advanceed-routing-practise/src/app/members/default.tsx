0f99d7948ff5ebe1f2d2c14ea15e52e397dc4ba5
manish_patidar <shanimpatidar@gmail.com>
2025-12-09 13:19:22 +0530

    paraller routes practise

--- 00-advanceed-routing-practise/src/app/members/default.tsx
+++ 00-advanceed-routing-practise/src/app/members/default.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 const AppDefault = () => {
-        return (
-        <div className='w-[300px] h-[100px] bg-purple-200 relative'>
-                <h1>App Default Page you can see here</h1>
-        </div>
-        )
+        return null
+        // return (
+        // <div className='w-[300px] h-[100px] bg-purple-200 relative'>
+        //         <h1>App Default Page you can see here</h1>
+        // </div>
+        // )
 }
 
 export default AppDefault
--- 00-advanceed-routing-practise/src/components/loading.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MembersLoading = () => {
-        return (
-                <div>MembersLoading ....</div>
-        )
-}
-
-export default MembersLoading
